0a77d3df17fe6bd72ce04919a2e960b65fcbd8b1
Brayarn Ogeda <brayarnogeda@gmail.com>
2024-08-31 09:54:40 +0300

    modify approach

--- src/components/Approach.jsx
+++ src/components/Approach.jsx
@@ -1,14 +1,15 @@
 const Approach = () => {
   return (
-    <div className="h-full bg-gradient-to-t from-[#087167] to-[#FAF5E0]">
-      <div className="mb-5 flex flex-col justify-center items-center">
+    <div className="h-full bg-[#FFFFFF]">
+      <div className="sm:ms-0 sm:me-0 ms-10 mb-5 flex flex-col justify-center items-center">
         <h1 className="mt-10 text-[#087167] text-[14px] font-semibold">
           Why Choose Us?
         </h1>
         <h1 className="sm:text-3xl text-2xl font-medium ">Our Approach</h1>
       </div>
-      <div className="sm:w-3/4 flex justify-around flex-wrap">
-        <div className="sm:w-1/3">
+
+      <div className="flex xl:justify-evenly md:justify-start flex-wrap gap-8 ms-10 me-10">
+        <div className="">
           <h1 className="font-bold text-xl">Virtual Forums</h1>
           <ul>
             <li>
@@ -20,7 +21,7 @@ const Approach = () => {
             </li>
           </ul>
         </div>
-        <div className="sm:w-1/3">
+        <div className="">
           <h1 className="font-bold text-xl">
             12-Week Self-Awareness Programme
           </h1>
