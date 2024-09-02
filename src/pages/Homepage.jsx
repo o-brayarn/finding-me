7ad0d341017def909f9d29c44a6f47c393bf2403
Brayarn Ogeda <brayarnogeda@gmail.com>
2024-09-02 16:00:25 +0300

    modify homepage bg-color

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -2,7 +2,7 @@ import { Approach, Hero } from "../components";
 
 const Homepage = () => {
   return (
-    <div className="h-full w-full">
+    <div className="h-full w-full bg-[#F5F5F5]">
       <Hero />
       <Approach />
     </div>
