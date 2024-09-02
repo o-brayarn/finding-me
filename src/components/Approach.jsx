de25a54329f36b755358b55d0655927bf052d492
Brayarn Ogeda <brayarnogeda@gmail.com>
2024-09-02 19:05:40 +0300

    modify approach section

--- src/components/Approach.jsx
+++ src/components/Approach.jsx
@@ -6,7 +6,7 @@ const Approach = () => {
     <div className="md:ms-10 md:me-10 md:rounded-xl  h-full bg-[#f0ffee] pb-10">
       <div className="md:flex justify-center items-center gap-36 mt-10 sm:pl-10 pl-5 pr-5">
         <div className="lg:w-1/4 md:w-1/2 flex flex-col">
-          <h1 className="mt-10 text-[#087167] text-[18  px] font-semibold">
+          <h1 className="mt-10 text-[#087167] text-[18px] font-semibold">
             Our Approach
           </h1>
           <h1 className="sm:text-3xl text-2xl font-bold mt-4">
@@ -28,9 +28,9 @@ const Approach = () => {
         </div>
 
         <div className="flex flex-col xl:justify-evenly md:justify-start flex-wrap gap-8  ">
-          <div className="text-[#FAF5E0] rounded-md bg-[#087167] p-4 shadow-2xl mt-6 md:mt-1 lg:w-4/5">
+          <div className="rounded-md p-4 backdrop-blur-2xl shadow-2xl mt-6 md:mt-1 lg:w-4/5">
             <h1 className="font-bold sm:text-2xl text-xl">Virtual Forums</h1>
-            <ul className="list-disc pl-4 ">
+            <ul className="list-disc hover:list-inside pl-4 ">
               <li className="pb-2 pt-2">
                 Knowledge acquisition from professionals in different fields.
               </li>
@@ -41,7 +41,7 @@ const Approach = () => {
             </ul>
           </div>
 
-          <div className="text-[#FAF5E0] rounded-md bg-[#087167] p-4 shadow-2xl lg:w-4/5">
+          <div className="rounded-md p-4 backdrop-blur-2xl shadow-2xl mt-6 md:mt-1 lg:w-4/5">
             <h1 className="font-bold sm:text-2xl text-xl">
               12-Week Self-Awareness Programme
             </h1>
