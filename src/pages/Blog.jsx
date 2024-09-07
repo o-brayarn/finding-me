1bceeadbd8d97d03e47115d8384373065b1f74e0
Brayarn Ogeda <brayarnogeda@gmail.com>
2024-09-07 15:03:10 +0300

    add blog header

--- src/pages/Blog.jsx
+++ src/pages/Blog.jsx
@@ -3,63 +3,63 @@ import { IoChevronDown } from "react-icons/io5";
 const blogData = [
   {
     title: "Aregan Ipsum Lorem",
-    src: "",
+    src: "https://images.pexels.com/photos/27871991/pexels-photo-27871991/free-photo-of-matterhorn.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load",
     date: "April 6, 2023",
     content:
       "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ad odio asperiores magni iste sit, voluptate eligendi aliquam distinctio nam excepturi.Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ad odio asperiores magni iste sit, voluptate eligendi aliquam distinctio nam excepturi.Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ad odio asperiores magni iste sit, voluptate eligendi aliquam distinctio nam excepturi.",
   },
   {
     title: "Aregan Ipsum Lorem",
-    src: "",
+    src: "https://images.pexels.com/photos/27550216/pexels-photo-27550216/free-photo-of-a-person-walking-through-the-fog-on-a-hill.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load",
     date: "April 6, 2023",
     content:
       "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ad odio asperiores magni iste sit, voluptate eligendi aliquam distinctio nam excepturi.Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ad odio asperiores magni iste sit, voluptate eligendi aliquam distinctio nam excepturi.Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ad odio asperiores magni iste sit, voluptate eligendi aliquam distinctio nam excepturi.",
   },
   {
     title: "Aregan Ipsum Lorem",
-    src: "",
+    src: "https://images.pexels.com/photos/27550216/pexels-photo-27550216/free-photo-of-a-person-walking-through-the-fog-on-a-hill.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load",
     date: "April 6, 2023",
     content:
       "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ad odio asperiores magni iste sit, voluptate eligendi aliquam distinctio nam excepturi.Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ad odio asperiores magni iste sit, voluptate eligendi aliquam distinctio nam excepturi.Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ad odio asperiores magni iste sit, voluptate eligendi aliquam distinctio nam excepturi.",
   },
   {
     title: "Aregan Ipsum Lorem",
-    src: "",
+    src: "https://images.pexels.com/photos/27550216/pexels-photo-27550216/free-photo-of-a-person-walking-through-the-fog-on-a-hill.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load",
     date: "April 6, 2023",
     content:
       "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ad odio asperiores magni iste sit, voluptate eligendi aliquam distinctio nam excepturi.Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ad odio asperiores magni iste sit, voluptate eligendi aliquam distinctio nam excepturi.Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ad odio asperiores magni iste sit, voluptate eligendi aliquam distinctio nam excepturi.",
   },
   {
     title: "Aregan Ipsum Lorem",
-    src: "",
+    src: "https://images.pexels.com/photos/27550216/pexels-photo-27550216/free-photo-of-a-person-walking-through-the-fog-on-a-hill.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load",
     date: "April 6, 2023",
     content:
       "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ad odio asperiores magni iste sit, voluptate eligendi aliquam distinctio nam excepturi.Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ad odio asperiores magni iste sit, voluptate eligendi aliquam distinctio nam excepturi.Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ad odio asperiores magni iste sit, voluptate eligendi aliquam distinctio nam excepturi.",
   },
   {
     title: "Aregan Ipsum Lorem",
-    src: "",
+    src: "https://images.pexels.com/photos/27550216/pexels-photo-27550216/free-photo-of-a-person-walking-through-the-fog-on-a-hill.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load",
     date: "April 6, 2023",
     content:
       "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ad odio asperiores magni iste sit, voluptate eligendi aliquam distinctio nam excepturi.Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ad odio asperiores magni iste sit, voluptate eligendi aliquam distinctio nam excepturi.Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ad odio asperiores magni iste sit, voluptate eligendi aliquam distinctio nam excepturi.",
   },
   {
     title: "Aregan Ipsum Lorem",
-    src: "",
+    src: "https://images.pexels.com/photos/27550216/pexels-photo-27550216/free-photo-of-a-person-walking-through-the-fog-on-a-hill.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load",
     date: "April 6, 2023",
     content:
       "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ad odio asperiores magni iste sit, voluptate eligendi aliquam distinctio nam excepturi.Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ad odio asperiores magni iste sit, voluptate eligendi aliquam distinctio nam excepturi.Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ad odio asperiores magni iste sit, voluptate eligendi aliquam distinctio nam excepturi.",
   },
   {
     title: "Aregan Ipsum Lorem",
-    src: "",
+    src: "https://images.pexels.com/photos/27550216/pexels-photo-27550216/free-photo-of-a-person-walking-through-the-fog-on-a-hill.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load",
     date: "April 6, 2023",
     content:
       "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ad odio asperiores magni iste sit, voluptate eligendi aliquam distinctio nam excepturi.Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ad odio asperiores magni iste sit, voluptate eligendi aliquam distinctio nam excepturi.Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ad odio asperiores magni iste sit, voluptate eligendi aliquam distinctio nam excepturi.",
   },
   {
     title: "Aregan Ipsum Lorem",
-    src: "",
+    src: "https://images.pexels.com/photos/27550216/pexels-photo-27550216/free-photo-of-a-person-walking-through-the-fog-on-a-hill.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load",
     date: "April 6, 2023",
     content:
       "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ad odio asperiores magni iste sit, voluptate eligendi aliquam distinctio nam excepturi.Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ad odio asperiores magni iste sit, voluptate eligendi aliquam distinctio nam excepturi.Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ad odio asperiores magni iste sit, voluptate eligendi aliquam distinctio nam excepturi.",
@@ -67,28 +67,50 @@ const blogData = [
 ];
 const Blog = () => {
   return (
-    <div>
-      <div>
-        <img src={blogData[0].src} alt={blogData[0].title.substring(0, 30)} />
-        <div>
-          <h1>{blogData[0].title}</h1>
-          <h3>{blogData[0].date}</h3>
+    <div className="w-full h-full">
+      <div className="md:mt-10 lg:ms-20 lg:me-20 sm:ms-10 sm:me-10 mt-5 ms-5 me-5 flex justify-center md:flex-nowrap flex-wrap gap-2 bg-[#087167] md:h-[350px] text-[#FAF5E0] md:pb-0 pb-4">
+        <div className="md:w-1/2 md:h-full h-64 w-full bg-slate-800 overflow-hidden">
+          <img
+            src={blogData[0].src}
+            alt={blogData[0].title.substring(0, 30)}
+            className="object-cover h-full w-full"
+          />
+        </div>
+        <div className="md:w-1/2 lg:ms-0 ms-2 lg:me-0 me-2">
+          <h1 className="font-bold lg:text-2xl text-xl">{blogData[0].title}</h1>
+          <h3 className="text-[14px] italic mb-2">{blogData[0].date}</h3>
           <p>{blogData[0].content}</p>
+          <button className="bg-[#FAF5E0] text-[#087167] font-bold px-3 py-2 mt-2 rounded-md pb-2">
+            Read More
+          </button>
         </div>
       </div>
-      <div>
-        <h1>
-          Category <IoChevronDown />
-        </h1>
-        <div>
-          <div>
+
+      <div className="h-full w-full">
+        <div className="flex justify-start sm:ms-10 ms-5 mt-10">
+          <h1 className="flex items-center bg-[#087167] px-4 text-[#FAF5E0] cursor-pointer">
+            <span className="font-bold sm:text-xl text-[18px]"> Category </span>
+            <IoChevronDown size={22} />
+          </h1>
+        </div>
+
+        <div className="md:mt-10 lg:ms-20 lg:me-20 sm:ms-10 sm:me-10 mt-5 ms-5 me-5 flex justify-center gap-2 bg-[#087167] md:h-[350px] text-[#FAF5E0] md:pb-0 pb-4">
+          <div className="md:w-1/2 md:h-full h-64 w-full bg-slate-800 overflow-hidden">
             <img
               src={blogData[0].src}
               alt={blogData[0].title.substring(0, 30)}
+              className="object-cover h-full w-full"
             />
-            <h1>{blogData[0].title}</h1>
-            <h3>{blogData[0].date}</h3>
+          </div>
+          <div className="md:w-1/2 lg:ms-0 ms-2 lg:me-0 me-2">
+            <h1 className="font-bold lg:text-2xl text-xl">
+              {blogData[0].title}
+            </h1>
+            <h3 className="text-[14px] italic mb-2">{blogData[0].date}</h3>
             <p>{blogData[0].content}</p>
+            <button className="bg-[#FAF5E0] text-[#087167] font-bold px-3 py-2 mt-2 rounded-md pb-2">
+              Read More
+            </button>
           </div>
         </div>
       </div>
