66bfc1bc621c0f1bb420bf293c37a7bd144a29fe
Brayarn Ogeda <brayarnogeda@gmail.com>
2024-09-08 21:01:52 +0300

    add contact page

---
+++ src/pages/Contact.jsx
@@ -0,0 +1,55 @@
+const Contact = () => {
+  return (
+    <div>
+      <h1 className="ms-10">Contact Us</h1>
+      <div className="ms-10">
+        <div>
+          <h1>
+            Interested to learn more about Finding Me Mental Health Wellness?
+          </h1>
+          <p>
+            Our team of experts is committed to helping you understand the
+            complexities of your mental health and that of those around you.
+            Contact us today to learn more about our mental health programs and
+            how we can help you navigate them.
+          </p>
+        </div>
+
+        <div>
+          <h1>Get in touch</h1>
+          <div>
+            <h2>By Phone</h2>
+            <p>Call us on (+274)701 103 302</p>
+          </div>
+          <div>
+            <h1>Email</h1>
+            <p>Send an email to hello@findingme.org</p>
+          </div>
+          <div>
+            <h1>Instant Messaging</h1>
+            <p>Connect via WhatsApp (+274)701 103 302</p>
+          </div>
+          <div>
+            <h1>Say Hello</h1>
+            <p>Office D3 -Ngong Lane Plaza</p>
+            <p>Ngong Road, Nairobi</p>
+            <p>Kenya</p>
+          </div>
+        </div>
+      </div>
+      <div>
+
+      <h1>Drop us a line...</h1>
+      <p>
+        Please send us an email with a bit about yourself and how we can be of
+        help. We will be in touch as soon as possible with some ideas.
+      </p>
+      <form>
+
+      </form>
+      </div>
+    </div>
+  );
+};
+
+export default Contact;
