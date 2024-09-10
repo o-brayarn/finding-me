d637c917eb72593aeef6c077f78e0c94b19c54bb
Brayarn Ogeda <brayarnogeda@gmail.com>
2024-09-10 19:12:11 +0300

    start team members page

--- src/pages/Team.jsx
+++ src/pages/Team.jsx
@@ -1,8 +1,104 @@
-
+const teamData = [
+  {
+    name: "Full name",
+    role: "Co-founder",
+    about:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ad oetur adipisicing elit. Ad odio asperiores magni iste sit, voluptate. Read more",
+    xaccount: "",
+    linkedInUrl: "",
+    src: "",
+  },
+  {
+    name: "Full name",
+    role: "Co-founder",
+    about:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ad oetur adipisicing elit. Ad odio asperiores magni iste sit, voluptate. Read more",
+    xaccount: "",
+    linkedInUrl: "",
+    src: "",
+  },
+  {
+    name: "Full name",
+    role: "Co-founder",
+    about:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ad oetur adipisicing elit. Ad odio asperiores magni iste sit, voluptate. Read more",
+    xaccount: "",
+    linkedInUrl: "",
+    src: "",
+  },
+  {
+    name: "Full name",
+    role: "Co-founder",
+    about:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ad oetur adipisicing elit. Ad odio asperiores magni iste sit, voluptate. Read more",
+    xaccount: "",
+    linkedInUrl: "",
+    src: "",
+  },
+  {
+    name: "Full name",
+    role: "Co-founder",
+    about:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ad oetur adipisicing elit. Ad odio asperiores magni iste sit, voluptate. Read more",
+    xaccount: "",
+    linkedInUrl: "",
+    src: "",
+  },
+  {
+    name: "Full name",
+    role: "Co-founder",
+    about:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ad oetur adipisicing elit. Ad odio asperiores magni iste sit, voluptate. Read more",
+    xaccount: "",
+    linkedInUrl: "",
+    src: "",
+  },
+  {
+    name: "Full name",
+    role: "Co-founder",
+    about:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ad oetur adipisicing elit. Ad odio asperiores magni iste sit, voluptate. Read more",
+    xaccount: "",
+    linkedInUrl: "",
+    src: "",
+  },
+  {
+    name: "Full name",
+    role: "Co-founder",
+    about:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ad oetur adipisicing elit. Ad odio asperiores magni iste sit, voluptate. Read more",
+    xaccount: "",
+    linkedInUrl: "",
+    src: "",
+  },
+];
 const Team = () => {
   return (
-    <div>Team</div>
-  )
-}
+    <div>
+      <div className="relative">
+        <div className="h-full w-full brightness-[0.3]">
+          <img
+            src="/images/volunteer.png"
+            alt=""
+            className="object-cover sm:h-[470px] h-full w-dvw"
+          />
+        </div>
+        <div className="absolute sm:top-[30%] top-[10%] text-[#FAF5E0] ms-5 flex flex-col justify-center items-center">
+          <h1 className="md:text-3xl text-2xl font-semibold">OUR TEAM</h1>
+          <p className="md:w-1/2">
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ad odio
+            asperiores magni iste sit, voluptate eligendi aliquam distinctio nam
+            excepturi.Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+            Ad odio asperiores{" "}
+          </p>
+        </div>
+      </div>
+      <div className="mt-10">
+        <h1 className="text-3xl font-bold text-center">Meet Our Team</h1>
+        <div className="flex justify-center"></div>
+      </div>
+    </div>
+  );
+};
 
-export default Team
+export default Team;
