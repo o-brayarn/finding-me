be021c69ff861da46bcd81bf569260a69e94f25a
Brayarn Ogeda <brayarnogeda@gmail.com>
2024-09-10 15:48:25 +0300

    add volunteer page cta

--- src/pages/Volunteer.jsx
+++ src/pages/Volunteer.jsx
@@ -16,6 +16,23 @@ const Volunteer = () => {
           </h2>
         </div>
       </div>
+
+      <div className="flex justify-start sm:gap-20 gap-8 flex-wrap mt-10 sm:ms-10 ms-5 sm:me-0 me-5">
+        <div className="md:w-1/4 sm:w-1/3 sm:block hidden border-2 border-[#06141B] p-2">
+          <img src="/images/about.png" className="object-cover h-full w-full" />
+        </div>
+        <div className="sm:w-1/2 flex flex-col gap-2 sm:gap-8 text-[#06141B]">
+          <p>
+            Are you a psychiatrist, a psychologist, a counselor or a therapist,
+            volunteer with us to mentor some of our members that may be going
+            through challenging mental health issues.
+          </p>
+          <p>
+            If interested in this opportunity please fill out the interest form
+            below.
+          </p>
+        </div>
+      </div>
     </div>
   );
 };
