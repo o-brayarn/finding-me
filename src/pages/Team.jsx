a2a66ccd165d8d0ec04138038db1f145a656a9c2
Brayarn Ogeda <brayarnogeda@gmail.com>
2024-09-13 16:28:18 +0300

    modify team members page

--- src/pages/Team.jsx
+++ src/pages/Team.jsx
@@ -1,78 +1,89 @@
+import { useState } from "react";
+
 const teamData = [
   {
+    id: 1,
     name: "Full name",
     role: "Co-founder",
     about:
-      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ad oetur adipisicing elit. Ad odio asperiores magni iste sit, voluptate. Read more",
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ad oetur adipisicing elit. Ad odio asperiores magni iste sit, voluptate.",
+    xaccount: "twitter",
+    linkedInUrl: "LI",
+    src: "https://images.pexels.com/photos/1222271/pexels-photo-1222271.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+  },
+  {
+    id: 2,
+    name: "Full name",
+    role: "Co-founder",
+    about:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ad oetur adipisicing elit. Ad odio asperiores magni iste sit, voluptate.",
     xaccount: "",
     linkedInUrl: "",
     src: "",
   },
   {
+    id: 3,
     name: "Full name",
     role: "Co-founder",
     about:
-      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ad oetur adipisicing elit. Ad odio asperiores magni iste sit, voluptate. Read more",
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ad oetur adipisicing elit. Ad odio asperiores magni iste sit, voluptate.",
     xaccount: "",
     linkedInUrl: "",
     src: "",
   },
   {
+    id: 4,
     name: "Full name",
     role: "Co-founder",
     about:
-      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ad oetur adipisicing elit. Ad odio asperiores magni iste sit, voluptate. Read more",
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ad oetur adipisicing elit. Ad odio asperiores magni iste sit, voluptate.",
     xaccount: "",
     linkedInUrl: "",
     src: "",
   },
   {
+    id: 5,
     name: "Full name",
     role: "Co-founder",
     about:
-      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ad oetur adipisicing elit. Ad odio asperiores magni iste sit, voluptate. Read more",
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ad oetur adipisicing elit. Ad odio asperiores magni iste sit, voluptate.",
     xaccount: "",
     linkedInUrl: "",
     src: "",
   },
   {
+    id: 6,
     name: "Full name",
     role: "Co-founder",
     about:
-      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ad oetur adipisicing elit. Ad odio asperiores magni iste sit, voluptate. Read more",
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ad oetur adipisicing elit. Ad odio asperiores magni iste sit, voluptate.",
     xaccount: "",
     linkedInUrl: "",
     src: "",
   },
   {
+    id: 7,
     name: "Full name",
     role: "Co-founder",
     about:
-      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ad oetur adipisicing elit. Ad odio asperiores magni iste sit, voluptate. Read more",
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ad oetur adipisicing elit. Ad odio asperiores magni iste sit, voluptate.",
     xaccount: "",
     linkedInUrl: "",
     src: "",
   },
   {
+    id: 8,
     name: "Full name",
     role: "Co-founder",
     about:
-      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ad oetur adipisicing elit. Ad odio asperiores magni iste sit, voluptate. Read more",
-    xaccount: "",
-    linkedInUrl: "",
-    src: "",
-  },
-  {
-    name: "Full name",
-    role: "Co-founder",
-    about:
-      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ad oetur adipisicing elit. Ad odio asperiores magni iste sit, voluptate. Read more",
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ad oetur adipisicing elit. Ad odio asperiores magni iste sit, voluptate.",
     xaccount: "",
     linkedInUrl: "",
     src: "",
   },
 ];
 const Team = () => {
+  const [isExpanded, setIsExpanded] = useState({});
   return (
     <div className="bg-[#F5F5F5]">
       <div className="relative">
@@ -95,13 +106,46 @@ const Team = () => {
       </div>
       <div className="mt-10">
         <h1 className="text-3xl font-bold text-center">Meet Our Team</h1>
-        <div className="flex justify-center">
-          <img src="" alt="" />
-          <h1>Name</h1>
-          <p>Role</p>
-          <p>about</p>
-          <a> X Account</a>
-          <a> linkedInUrl</a>
+        <div className="mt-10 flex justify-center gap-8 flex-wrap">
+          {teamData &&
+            teamData.map((team) => {
+              return (
+                <div
+                  key={team.id}
+                  className="lg:w-1/5 md:w-1/3 sm:w-1/2 w-3/4 border-2 p-2"
+                >
+                  <img src={team.src} alt={team.name} />
+                  <div className="flex justify-between gap-4 mt-2">
+                    <h1>
+                      {team.name}, <span>{team.role}</span>
+                    </h1>
+                    <div className="flex gap-2">
+                      <a> {team.xaccount}</a>
+                      <a>{team.linkedInUrl}</a>
+                    </div>
+                  </div>
+
+                  <p className="mt-2">
+                    {isExpanded[team.id]
+                      ? team.about
+                      : team.about.substring(0, 120)}
+                    ...
+                  </p>
+
+                  <button
+                    className="text-[#087167]"
+                    onClick={() =>
+                      setIsExpanded((prev) => ({
+                        ...prev,
+                        [team.id]: !prev[team.id],
+                      }))
+                    }
+                  >
+                    {isExpanded[team.id] ? "Read Less" : "Read More"}
+                  </button>
+                </div>
+              );
+            })}
         </div>
       </div>
     </div>
