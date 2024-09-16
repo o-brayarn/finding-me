a1649c07a155c04d07ea01694eec4b2cd61399aa
Brayarn Ogeda <brayarnogeda@gmail.com>
2024-09-16 14:11:25 +0300

    add history page - complete header

--- src/pages/History.jsx
+++ src/pages/History.jsx
@@ -1,8 +1,22 @@
-
 const History = () => {
   return (
-    <div>History</div>
-  )
-}
+    <div>
+      <div className="relative">
+        <div className="h-full w-full brightness-[0.3]">
+          <img
+            src="/images/volunteer.png"
+            alt=""
+            className="object-cover sm:h-[470px] h-full w-dvw"
+          />
+        </div>
+        <div className="w-full absolute sm:top-[50%] top-[40%] text-[#FAF5E0] ms-5 flex justify-center items-center">
+          <h1 className="md:text-3xl text-2xl font-semibold">
+            HISTORY OF FINDING ME
+          </h1>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default History
+export default History;
