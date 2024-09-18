bab4967f5908bb414ffd9b29783c42f40c6412fd
Brayarn Ogeda <brayarnogeda@gmail.com>
2024-09-18 09:14:33 +0300

    update content to scroll to top

--- src/App.jsx
+++ src/App.jsx
@@ -14,9 +14,23 @@ import {
   Volunteer,
 } from "./pages";
 
+import { useEffect } from "react";
+import { useLocation } from "react-router-dom";
+
+const ScrollToTop = () => {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
+  return null;
+};
+
 function App() {
   return (
     <Router>
+      <ScrollToTop />
       <Navbar />
       <Routes>
         <Route path="/" element={<Homepage />} />
